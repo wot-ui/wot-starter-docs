a2443f141dc380a42bd896169dc9620157eb7dcd
不如摸鱼去 <1780903673@qq.com>
2025-10-30 14:07:41 +0800

    update banner

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,10 +1,10 @@
 /*
  * @Author: weisheng
  * @Date: 2024-10-12 22:09:33
- * @LastEditTime: 2025-09-03 21:16:42
+ * @LastEditTime: 2025-10-30 14:06:33
  * @LastEditors: weisheng
  * @Description: 
- * @FilePath: /wot-starter-docs/.vitepress/theme/index.ts
+ * @FilePath: /wot-demo-docs/.vitepress/theme/index.ts
  * 记得注释
  */
 import { h } from 'vue'
@@ -19,6 +19,8 @@ import SvgImage from './components/SvgImage.vue'
 import HomeStar from './components/HomeStar.vue'
 import VPIframe from './components/VPIframe.vue'
 import WwAds from './components/WwAds.vue'
+import Banner from './components/Banner.vue'
+
 
 export default {
   ...Theme,
@@ -26,6 +28,7 @@ export default {
     return h(Theme.Layout, null, {
       // 在首页 hero 部分信息后添加 star 徽章
       'home-hero-info-after': () => h(HomeStar),
+      'layout-top': () => h(Banner),
       'aside-outline-after': () => h(WwAds),
     })
   },
